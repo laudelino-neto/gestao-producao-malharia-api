85a851290d0f1a2e28f7aa9cef479e3436474794
Eduarda Manenti <eduarda_manenti@nuntec.com.br>
2023-12-12 21:48:21 -0300

    Finalizado a entidade.

--- src/main/java/br/com/gestaoproducaomalharia/entity/Colaborador.java
+++ src/main/java/br/com/gestaoproducaomalharia/entity/Colaborador.java
@@ -1,19 +1,59 @@
 package br.com.gestaoproducaomalharia.entity;
 
 import br.com.gestaoproducaomalharia.entity.enums.Status;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "colaboradores")
+@Entity(name = "Colaborador")
 public class Colaborador {
 
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	@EqualsAndHashCode.Include
 	private Integer id;
 	
+	@NotBlank(message = "O nome é obrigatório.")
+	@Column(name = "nome_completo")
 	private String nomeCompleto;
 	
+	@Column(name = "telefone")
 	private String telefone;
 	
+	@Column(name = "email")
 	private String email;
 	
+	@NotNull(message = "O status do colaborador é obrigatório.")
+	@Enumerated(value = EnumType.STRING)
+	@Column(name = "status")
 	private Status status;
+	
+	public Colaborador() {
+		this.status = Status.A;
+	}
+	
+	@Transient
+	public boolean isPersistido() {
+		return getId() != null && getId() > 0;
+	}
+	
+	@Transient
+	public boolean isAtivo() {
+		return getStatus() == Status.A;
+	}
 	
 }
