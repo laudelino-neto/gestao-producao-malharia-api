2472ca2109b9b7c498a3bddf2f8e6e24df52e44b
Eduarda Manenti <eduarda_manenti@nuntec.com.br>
2023-12-12 21:49:17 -0300

    Finalizado o repositório.

---
+++ src/main/java/br/com/gestaoproducaomalharia/repository/ColaboradoresRepository.java
@@ -0,0 +1,32 @@
+package br.com.gestaoproducaomalharia.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import br.com.gestaoproducaomalharia.entity.Colaborador;
+import br.com.gestaoproducaomalharia.entity.enums.Status;
+
+@Repository
+public interface ColaboradoresRepository extends JpaRepository<Colaborador, Integer> {
+	
+	@Query(value = "SELECT c FROM Colaborador c WHERE Upper(c.nomeCompleto) LIKE Upper(:nomeCompleto) ORDER BY c.nomeCompleto ", 
+			countQuery = "SELECT Count(c) FROM Colaborador c WHERE Upper(c.nomeCompleto) LIKE Upper(:nomeCompleto)")
+	public Page<Colaborador> listarPor(String nomeCompleto, Pageable paginacao);
+	
+	@Query(value = "SELECT c FROM Colaborador c WHERE c.id = :id")
+	public Colaborador buscarPor(Integer id);
+	
+	@Query(value = "SELECT c " 
+			+ "FROM Colaborador c " 
+			+ "WHERE Upper(c.nomeCompleto) = Upper(:nomeCompleto)")
+	public Colaborador buscarPor(String nomeCompleto);
+	
+	@Modifying
+	@Query(value = "UPDATE Colaborador c SET c.status = :status WHERE c.id = :id")
+	public void atualizarStatusPor(Integer id, Status status);
+
+}
