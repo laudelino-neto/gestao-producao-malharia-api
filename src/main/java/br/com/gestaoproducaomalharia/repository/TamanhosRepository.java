c1f04c47150533b8b73897a6dff0e24892e6d183
eduarda-manentii <dudamanenti65@gmail.com>
2023-12-13 09:17:55 -0300

    Criado o repositório de Tamanho.

---
+++ src/main/java/br/com/gestaoproducaomalharia/repository/TamanhosRepository.java
@@ -0,0 +1,26 @@
+package br.com.gestaoproducaomalharia.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import br.com.gestaoproducaomalharia.entity.Tamanho;
+import br.com.gestaoproducaomalharia.entity.enums.Status;
+
+@Repository
+public interface TamanhosRepository extends JpaRepository<Tamanho, Integer>{
+
+	@Query(value = "SELECT t FROM Tamanho t WHERE t.id = :id")
+	public Tamanho buscarPor(Integer id);
+	
+	@Query(value = "SELECT t " 
+			+ "FROM Tamanho t " 
+			+ "WHERE Upper(t.sigla) = Upper(:sigla)")
+	public Tamanho buscarPor(String sigla);
+	
+	@Modifying
+	@Query(value = "UPDATE Tamanho t SET t.status = :status WHERE t.id = :id")
+	public void atualizarStatusPor(Integer id, Status status);
+
+}
