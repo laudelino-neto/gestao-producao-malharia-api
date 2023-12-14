9883cae8dae4111726b0a0c498913ec173f50aaf
eduarda-manentii <dudamanenti65@gmail.com>
2023-12-14 14:06:54 -0300

    Adicionado validações do acerto na interface service.

--- src/main/java/br/com/gestaoproducaomalharia/service/EscalaService.java
+++ src/main/java/br/com/gestaoproducaomalharia/service/EscalaService.java
@@ -8,11 +8,25 @@ import org.springframework.validation.annotation.Validated;
 
 import br.com.gestaoproducaomalharia.entity.AcertoDeEscala;
 import br.com.gestaoproducaomalharia.entity.Escala;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 @Validated
 public interface EscalaService {
 
-	public AcertoDeEscala salvar(AcertoDeEscala acerto);
+	public AcertoDeEscala salvar(
+			@NotNull(message = "O acerto é obrigatório.") 
+			AcertoDeEscala acerto);
+	
+	public AcertoDeEscala excluirPor(
+			@NotNull(message = "O id para a exclusão é obrigatório.") 
+			@Positive(message = "O id para a exclusão deve ser positivo.")
+			Integer id);
+	
+	public AcertoDeEscala buscarPor(
+			@NotNull(message = "O id é obrigatório.") 
+			@Positive(message = "O id deve ser positivo.")
+			Integer id);
 	
 	public Escala salvar(Escala escala);
 	
